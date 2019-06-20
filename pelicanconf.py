7721dd158483ac48f0db61530e9cd05b9b4b48fe
Patrick A. Levell <patrick.a.levell@gmail.com>
2019-06-20 12:07:29 -0400

    Updated Site Logo

---
+++ pelicanconf.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+from __future__ import unicode_literals
+
+AUTHOR   = 'Patrick'
+SITENAME = 'Pelican'
+SITEURL  = ''  # 'https://palevell.github.io/pelican'
+# THEME ='random_theme'
+THEME = 'pelican-themes/Flex'
+# BOOTSTRAP_THEME = ''  # 'united'
+# PYGMENTS_STYLE = 'native'
+
+PATH = 'content'
+TIMEZONE = 'America/Toronto'
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (("People's Party of Canada", 'https://www.peoplespartyofcanada.ca/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('@peoplespca', 'https://twitter.com/@peoplespca'),
+          ('@MaximeBernier', 'https://twitter.com/@MaximeBernier'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Ref: https://pythonforundergradengineers.com/how-i-built-this-site-3.html
+PLUGIN_PATHS = [ 'pelican-plugins', ]
+PLUGINS = ['i18n_subsites', ]
+
+STATIC_PATHS = [
+    'images',
+    'extra/robots.txt',
+    'extra/favicon.ico'
+]
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'}
+}
+
+## Theme-specific settings
+## Pelican Twitchy
+SITESUBTITLE = 'Site sub-title'
+EXPAND_LATEST_ON_INDEX = True
+SHARE = False
+DISPLAY_TAGS_ON_MENU = False
+COOKIE_CONSENT = True
+TYPOGRIFY = True
+
+## Flex
+# SITETITLE = 'Foo Bar'
+SITESUBTITLE = 'Web Developer'
+SITEDESCRIPTION = 'Foo Bar\'s Thoughts and Writings'
+SITELOGO = '/images/ppc-logo-03.png'
+# FAVICON = SITEURL + '/images/favicon.ico'
+
+BROWSER_COLOR = '#333'
+ROBOTS = 'index, follow'
+
+
+
